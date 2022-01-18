5f4db451febafb4956abcc4b7854d1d42855ad18
Sadzony <szymon.sadunski@gmail.com>
2022-01-18 23:10:47 +0000

    Fix Terrain Colission

--- Ergate/Assets/Pablo_Folder/Assets/Scripts/CameraTerrainCollision.cs
+++ Ergate/Assets/Pablo_Folder/Assets/Scripts/CameraTerrainCollision.cs
@@ -11,44 +11,26 @@ public class CameraTerrainCollision : MonoBehaviour
 
     private void Start()
     {
-        m_offset = transform.position - transform.parent.position;
-        Debug.Log("offset: " + m_offset);
+        m_offset = transform.localPosition; //calculate the offset vector at start
     }
 
     private void Update()
     {
-        Vector3 originalCameraPosition = transform.parent.position + m_offset;
-        originalCameraPosition =  transform.parent.rotation * originalCameraPosition;
-        Debug.DrawLine(m_Target.position, transform.position, Color.black);
+        Vector3 originalCameraPosition = transform.parent.position + (transform.parent.localRotation * m_offset); //calculate the pre-terrain collision position
+        Debug.DrawLine(transform.parent.position, originalCameraPosition, Color.black);
         ///Explanation of this script
         ///It creates a linecast from the cameraholder to the camera, and if the lineCast hits an object with "Ground" layer, 
         ///then the position of the camera is equal to the point of the raycast hit
-        bool positionHit = Physics.Linecast(m_Target.position, transform.position, out hit, m_Ground);
-        bool originalPositionHit = Physics.Linecast(m_Target.position, originalCameraPosition, out hit, m_Ground);
-        
+        bool originalPositionHit = Physics.Linecast(transform.parent.position, originalCameraPosition, out hit, m_Ground);
 
-        if (positionHit && originalPositionHit)
+        if (originalPositionHit)
         {
-            Debug.Log("Terrain collision");
-            Vector3 hitVector = transform.position - hit.point;
-            Debug.DrawLine(m_Target.position, (hit.point * 0.87f), Color.red);
-            //Debug.Log(hit.distance);
-            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z + hitVector.magnitude + 0.1f);
-            //transform.position = transform.position - hitVector;
+            transform.position = hit.point*0.95f;
 
         }
-        
-        if (originalPositionHit && !positionHit)
+        else
         {
-            //transform.position = (hit.point * 0.95f);
-        }
-
-        Debug.Log("original position hit: " + originalPositionHit);
-        Debug.Log("current position hit: " + positionHit);
-        if (!positionHit && !originalPositionHit)
-        {
-            Debug.Log("Camera Position Reset");
-            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, -4.85f);
+            transform.localPosition = m_offset;
         }
     } 
 }
