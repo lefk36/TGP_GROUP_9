ad7766848b72affe25ff58ac0efb770b6c00fd80
Pablo Cano San Román <70590054+pablocanosanroman@users.noreply.github.com>
2022-01-18 18:18:26 +0000

    Changes to the camera, terrain collisions and structure of the camera

--- Ergate/Assets/Pablo_Folder/Assets/Scripts/Camera.cs
+++ Ergate/Assets/Pablo_Folder/Assets/Scripts/Camera.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-    public Vector3 m_LocalCameraOffSet; //Position of the camera from the player.
-    [SerializeField] private Transform m_TrackedObjectTransform; //Object that the camera will follow
-    [SerializeField] private Transform m_Camera; //Camera Transform
-    [SerializeField] private float m_AutoCamSpeed; //Speed in which the camra follows the player
-
+    
     [Range(0f, 2000f)] //Made it a range so it can be changed in inspector
-    [Min(0f)][SerializeField] private float m_Sensitivity; //Sensitivity of the camera
+    [Min(0f)][SerializeField] private float m_MouseSensitivity; //Mouse Sensitivity of the camera
+
+    [Range(0f, 2000f)]
+    [Min(0f)] [SerializeField] private float m_ControllerSensitivity; //Controller Sensitivity of the camera
 
     [Range(0f, 80f)]
     [Min(0f)][SerializeField] private float m_ClampAngle;//Limit angle movement X rotation
@@ -20,13 +19,14 @@ public class Camera : MonoBehaviour
     //Controller Input Variables
     private float m_ControllerHorizontal;
     private float m_ControllerVertical;
-    //Input of both mouse and Controller
-    private float m_MixedInputX;
-    private float m_MixedInputY;
     //Rotation X axis
-    [SerializeField] private float m_RotationX;
+    private float m_MouseRotationX;
     //ROtation Y Axis
-    [SerializeField] private float m_RotationY;
+    private float m_MouseRotationY;
+    //Rotation X axis(Controller)
+    private float m_ControllerRotationX;
+    //ROtation Y Axis(Controller)
+    private float m_ControllerRotationY;
     //List of enemies located
     List<GameObject> m_EnemiesLocated = new List<GameObject>();
 
@@ -35,8 +35,12 @@ public class Camera : MonoBehaviour
         //vector of the camera holder rotation
         Vector3 rotation = transform.localRotation.eulerAngles;
         //Set the x and y rotation to the variables made for that
-        m_RotationX = rotation.x;
-        m_RotationY = rotation.y;
+        m_MouseRotationX = rotation.x;
+        m_MouseRotationY = rotation.y;
+
+        m_ControllerRotationX = rotation.x;
+        m_ControllerRotationY = rotation.y;
+
         //Lock the cursor to not make the camera go around crazy
         Cursor.lockState = CursorLockMode.Locked;
         //Makes the cursor invisible
@@ -55,47 +59,53 @@ public class Camera : MonoBehaviour
         m_ControllerVertical = Input.GetAxis("ControllerVertical");
         m_MouseX = Input.GetAxis("Mouse X");
         m_MouseY = Input.GetAxis("Mouse Y");
-        //Mixes both mouse and controller input, so I dont put the code twice
-        m_MixedInputX = m_MouseX + m_ControllerHorizontal;
-        m_MixedInputY = -m_MouseY + m_ControllerVertical;
+        
 
         //Sets the rotations to the input multiplied by the sensitivity to control how fast the camera moves
-        m_RotationY += m_MixedInputX * m_Sensitivity * Time.deltaTime;
-        m_RotationX += m_MixedInputY * m_Sensitivity * Time.deltaTime;
+        m_MouseRotationY += m_MouseX * m_MouseSensitivity * Time.deltaTime;
+        m_MouseRotationX += -m_MouseY * m_MouseSensitivity * Time.deltaTime;
+
+        m_ControllerRotationY += m_ControllerHorizontal * m_ControllerSensitivity * Time.deltaTime;
+        m_ControllerRotationX += m_ControllerVertical * m_ControllerSensitivity * Time.deltaTime;
 
         //Limits the rotation on the x axis
-        m_RotationX = Mathf.Clamp(m_RotationX, -m_ClampAngle, m_ClampAngle);
+        m_MouseRotationX = Mathf.Clamp(m_MouseRotationX, -m_ClampAngle, m_ClampAngle);
+        m_ControllerRotationX = Mathf.Clamp(m_ControllerRotationX, -m_ClampAngle, m_ClampAngle);
 
-        //Sets the rotation of the camera holder to the rotation on the x and y axis
-        transform.rotation = Quaternion.Euler(m_RotationX, m_RotationY, 0f);
-        //Sets the position of the camera holder always in the character
-        transform.position = m_TrackedObjectTransform.parent.position;
-
+        //Sets the rotation of the camera holder to the rotation on the x and y axis depending on if the player is using the mouse or a controller
+        if(Input.GetAxis("Mouse X") < 0.1f || Input.GetAxis("Mouse X") > 0.1f || Input.GetAxis("Mouse Y") < 0.1f || Input.GetAxis("Mouse Y") > 0.1f)
+        {
+            transform.rotation = Quaternion.Euler(m_MouseRotationX, m_MouseRotationY, 0f);
+        }
+        else if(Input.GetAxis("ControllerHorizontal") < 0.1f || Input.GetAxis("ControllerHorizontal") > 0.1f || Input.GetAxis("ControllerVertical") < 0.1f || Input.GetAxis("ControllerVertical") > 0.1f)
+        {
+            transform.rotation = Quaternion.Euler(m_ControllerRotationX, m_ControllerRotationY, 0f);
+        }
         
         
     }
 
-    //I put it on FixedUpdate because in Late it makes the camera jittery
-    private void FixedUpdate()
-    {
-        //AutoCamera
-        float worldYRotInRad = m_Camera.rotation.eulerAngles.y * Mathf.Deg2Rad; //The y rotation of the camera in radians
-        //Current position of the camera
-        Vector3 currentCameraPosition = m_Camera.position - new Vector3(m_LocalCameraOffSet.z * Mathf.Sin(worldYRotInRad) + m_LocalCameraOffSet.x * Mathf.Cos(worldYRotInRad),
-                                                                    m_LocalCameraOffSet.y,
-                                                                    m_LocalCameraOffSet.z * Mathf.Cos(worldYRotInRad) - m_LocalCameraOffSet.x * Mathf.Sin(worldYRotInRad));
+    ////I put it on FixedUpdate because in Late it makes the camera jittery
+    //private void FixedUpdate()
+    //{
+    //    //AutoCamera
+    //    float worldYRotInRad = m_Camera.rotation.eulerAngles.y * Mathf.Deg2Rad; //The y rotation of the camera in radians
+    //    //Current position of the camera
+    //    Vector3 currentCameraPosition = m_Camera.position - new Vector3(m_LocalCameraOffSet.z * Mathf.Sin(worldYRotInRad) + m_LocalCameraOffSet.x * Mathf.Cos(worldYRotInRad),
+    //                                                                m_LocalCameraOffSet.y,
+    //                                                                m_LocalCameraOffSet.z * Mathf.Cos(worldYRotInRad) - m_LocalCameraOffSet.x * Mathf.Sin(worldYRotInRad));
 
-        //Position of the offSetCamera
-        Vector3 cameraOffSetPosition = m_Camera.position - currentCameraPosition;
+    //    //Position of the offSetCamera
+    //    Vector3 cameraOffSetPosition = m_Camera.position - currentCameraPosition;
 
-        //Distance from the camera to the target
-        float distToTarget = (m_TrackedObjectTransform.position - currentCameraPosition).magnitude;
+    //    //Distance from the camera to the target
+    //    float distToTarget = (m_TrackedObjectTransform.position - currentCameraPosition).magnitude;
 
-        //Makes the camera follow the player
-        m_Camera.position = Vector3.MoveTowards(currentCameraPosition, m_TrackedObjectTransform.position, distToTarget * m_AutoCamSpeed * Time.fixedDeltaTime) + cameraOffSetPosition;
+    //    //Makes the camera follow the player
+    //    m_Camera.position = Vector3.MoveTowards(currentCameraPosition, m_TrackedObjectTransform.position, distToTarget * m_AutoCamSpeed * Time.fixedDeltaTime) + cameraOffSetPosition;
 
 
-    }
+    //}
 
 
 }
