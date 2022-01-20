e4051fd02edea24cb349f17e3171d955091d9772
Sadzony <szymon.sadunski@gmail.com>
2022-01-20 17:36:08 +0000

    Revert "Merge remote-tracking branch 'origin/Pablo_LockCamera' into Weapon-Wheel"
    This reverts commit 704d8b6ffd435f3dd427d808d3d9a334484ee479, reversing
    changes made to 34694fcebb43024f57c7d1e02b09585d2b5b257e.

--- Ergate/Assets/Pablo_Folder/Assets/Scripts/Camera_Movement.cs
+++ Ergate/Assets/Pablo_Folder/Assets/Scripts/Camera_Movement.cs
@@ -27,10 +27,6 @@ public class Camera_Movement : MonoBehaviour
     private float m_ControllerRotationX;
     //ROtation Y Axis(Controller)
     private float m_ControllerRotationY;
-    //Initial Rotation
-    private Vector3 m_InitialRotation;
-    //Turn Smooth rotation
-    [SerializeField] private float m_TurnSmoothRotation;
 
 
     private void Start()
@@ -41,14 +37,13 @@ public class Camera_Movement : MonoBehaviour
     private void OnEnable()
     {
         //vector of the camera holder rotation
-        transform.localRotation = Quaternion.Euler(0, 0, 0);
-        m_InitialRotation = transform.localRotation.eulerAngles;
+        Vector3 rotation = transform.localRotation.eulerAngles;
         //Set the x and y rotation to the variables made for that
-        m_MouseRotationX = m_InitialRotation.x;
-        m_MouseRotationY = m_InitialRotation.y;
+        m_MouseRotationX = rotation.x;
+        m_MouseRotationY = rotation.y;
 
-        m_ControllerRotationX = m_InitialRotation.x;
-        m_ControllerRotationY = m_InitialRotation.y;
+        m_ControllerRotationX = rotation.x;
+        m_ControllerRotationY = rotation.y;
 
         //Lock the cursor to not make the camera go around crazy
         Cursor.lockState = CursorLockMode.Locked;
