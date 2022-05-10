e8117e4f0dd1cc13b62652f1193d31ff50dad517
pablocanosanroman <c015653k@student.staffs.ac.uk>
2022-05-10 21:31:03 +0100

    Simple AI working, things need to be fixed though

--- Ergate/Assets/Pablo_Folder/Assets/Scripts/Zombie.cs
+++ Ergate/Assets/Pablo_Folder/Assets/Scripts/Zombie.cs
@@ -8,9 +8,39 @@ public class Zombie : BaseEnemy
     private void Start()
     {
         m_Health = 100f;
-        m_Damage = 20f;
-        m_ZombieAgent = GetComponent<NavMeshAgent>();
+        m_HealthDamage = 20;
+        m_PoiseDamage = 20;
+        m_PlayerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerPoiseAndHealth>();
+        m_Animator = GetComponent<Animator>();
+        m_Agent = GetComponent<NavMeshAgent>();
+        m_Target = GameObject.FindGameObjectWithTag("Player");
+        m_CanAttack = true;
     }
 
+    private void Update()
+    {
+        FacePlayer();
+        SetEnemyPath();
 
+        Vector3 enemyToPlayer = m_Target.transform.position - transform.position;
+        if (enemyToPlayer.magnitude < m_Agent.stoppingDistance && m_CanAttack)
+        {
+            if (m_Target.CompareTag("Player"))
+            {
+                DealDamage(Vector3.zero, m_HealthDamage, m_PoiseDamage);
+                m_CanAttack = false;
+                StartCoroutine(AttackReset());
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+    IEnumerator AttackReset()
+    {
+        yield return new WaitForSeconds(m_AttackRate);
+        m_CanAttack = true;
+    }
 }
