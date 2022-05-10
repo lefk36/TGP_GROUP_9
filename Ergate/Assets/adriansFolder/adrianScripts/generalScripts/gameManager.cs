7d998f7e8ccd1557df80d8605b6ca90fe9739f78
Sadzony <szymon.sadunski@gmail.com>
2022-05-10 19:28:33 +0100

    Fix GameManager bug with audio controller trying to be accessed before the playerScene is loaded

--- Ergate/Assets/adriansFolder/adrianScripts/generalScripts/gameManager.cs
+++ Ergate/Assets/adriansFolder/adrianScripts/generalScripts/gameManager.cs
@@ -7,16 +7,31 @@ public class gameManager : MonoBehaviour
     public audioController m_audioController;
     public GameObject m_audioManager;
 
+    private Scene playerScene;
+
     private void Awake()
     {
         SceneManager.LoadScene("playerScene", LoadSceneMode.Additive);
-        m_audioManager = FindObjectOfType<audioController>().gameObject;
-        m_audioController = m_audioManager.GetComponent<audioController>();
-        inGameMusic();
+        playerScene = SceneManager.GetSceneByName("playerScene");
+        StartCoroutine(InitiateAfterSceneLoad());
     }
 
     public void inGameMusic()
     {
         m_audioController.play("musicOne");
     }
+    IEnumerator InitiateAfterSceneLoad()
+    {
+        while (!playerScene.isLoaded)
+        {
+            yield return null;
+        }
+        if (m_audioManager == null)
+        {
+            m_audioManager = FindObjectOfType<audioController>().gameObject;
+        }
+        m_audioController = m_audioManager.GetComponent<audioController>();
+        inGameMusic();
+    }
+    
 }
