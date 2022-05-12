926e96d87d2dd894a6b27454dc021fe5efd2cbfb
Sadzony <szymon.sadunski@gmail.com>
2022-05-12 11:41:34 +0100

    Contrinue combo implementation

--- Ergate/Assets/Scripts/Player/AttackSystem/Attacks/AttackData.cs
+++ Ergate/Assets/Scripts/Player/AttackSystem/Attacks/AttackData.cs
@@ -8,6 +8,7 @@ public class AttackData : ScriptableObject
 {
     [SerializeField] private MonoScript thisAttackState;
     [HideInInspector] public AttackState state;
+    private AttackData transitionAttackData;
     public List<AttackData> chainableAttacks;
     private void OnEnable()
     {
@@ -18,4 +19,15 @@ public class AttackData : ScriptableObject
     public bool holdingRequired;
     public float timeHeldRequired;
     public string buttonRequired;
+
+    public virtual void ChainCombo(AttackData p_transitionAttackData)
+    {
+        transitionAttackData = p_transitionAttackData;
+    }
+    public virtual AttackData CheckTransitions()
+    {
+        //when the attack is finished, it changes the variable returned in this function (if a combo chain happened)
+        return null;
+    }
+
 }
--- Ergate/Assets/Scripts/Player/AttackSystem/Attacks/AttackState.cs
+++ Ergate/Assets/Scripts/Player/AttackSystem/Attacks/AttackState.cs
@@ -10,22 +10,16 @@ public class AttackState
     public virtual void StartAttack(Weapon caller)
     {
         //start the coroutine on the caller MonoBehaviour
-    }
-    public virtual void ChainCombo(string button)
-    {
-        //process the button string. If the attack allows for a chain combo with the button, prepare the program to change states
-    }
-    public virtual AttackState CheckTransitions()
-    {
-        //when the attack is finished, it changes the variable returned in this function (if a combo chain happened)
-        return null;
+        caller.StartCoroutine(AttackCoroutine());
     }
     public virtual void CancelAttack(MonoBehaviour caller)
     {
         //stop the coroutine reference on the caller;
+        caller.StopCoroutine(AttackCoroutine());
     }
     protected virtual IEnumerator AttackCoroutine()
     {
+        completed = true;
         yield return null;
     }
 }
