be0ea7e0072ca1b2e3a50663b906b491404fc9b3
lefk36 <57408683+lefk36@users.noreply.github.com>
2022-05-23 17:29:20 +0100

    revert

--- Ergate/Assets/charleytemp/Health and Poise/PlayerPoiseAndHealth.cs
+++ Ergate/Assets/charleytemp/Health and Poise/PlayerPoiseAndHealth.cs
@@ -56,7 +56,6 @@ public class PlayerPoiseAndHealth : MonoBehaviour
 
     private void Start()
     {
-        HealthBar.instance.SetupCubes(10);
         m_PlayerSpawn = GetComponent<playerSpawn>();
         m_PlayerController = GetComponent<PlayerController>();
         m_Character = GameObject.Find("Character");
@@ -135,7 +134,7 @@ public class PlayerPoiseAndHealth : MonoBehaviour
     //}
     public void TakeDamage(Vector3 attackDirection, int healthDamageAmount, int poiseDamageAmount)
     {
-        HealthBar.instance.RemoveHealth(healthDamageAmount);
+      
         m_PlayerAnimator.SetTrigger("TakeDamage");
         Debug.Log("okayer damage taken");
         rb.AddForce(attackDirection, ForceMode.Impulse);
