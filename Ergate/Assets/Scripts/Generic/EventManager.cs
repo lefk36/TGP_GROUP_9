04d3eecd72c6d16da7e81bbecd7e606532947da8
Pablo Cano San Román <70590054+pablocanosanroman@users.noreply.github.com>
2022-05-23 13:06:30 +0100

    EventManager Commented

--- Ergate/Assets/Scripts/Generic/EventManager.cs
+++ Ergate/Assets/Scripts/Generic/EventManager.cs
@@ -3,43 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class EventManager : MonoBehaviour
-{
+//public class EventManager : MonoBehaviour
+//{
 
-    public static EventManager current;
+//    //public static EventManager current;
 
-    // Start is called before the first frame update
-    void Awake()
-    {
-        current = this;
-    }
+//    //// Start is called before the first frame update
+//    //void Awake()
+//    //{
+//    //    current = this;
+//    //}
 
 
-    public event Action onEnemyDamage;
-    public void EnemyDamage()
-    {
-        if (onEnemyDamage != null)
-        {
-            onEnemyDamage();
-        }
-    }
+//    //public event Action onEnemyDamage;
+//    //public void EnemyDamage()
+//    //{
+//    //    if (onEnemyDamage != null)
+//    //    {
+//    //        onEnemyDamage();
+//    //    }
+//    //}
 
-    public event Action<Vector3, int, int> onPlayerDamage;
-    public void PlayerDamage(Vector3 attackDirection, int healthDamageAmount, int poiseDamageAmount)
-    {
-        if (onPlayerDamage != null)
-        {
-            onPlayerDamage(attackDirection, healthDamageAmount, poiseDamageAmount);
-        }
-    }
+//    //public event Action<Vector3, int, int> onPlayerDamage;
+//    //public void PlayerDamage(Vector3 attackDirection, int healthDamageAmount, int poiseDamageAmount)
+//    //{
+//    //    if (onPlayerDamage != null)
+//    //    {
+//    //        onPlayerDamage(attackDirection, healthDamageAmount, poiseDamageAmount);
+//    //    }
+//    //}
 
-    public event Action onEnemyDestroy;
-    public void DestroyEnemy()
-    {
-        if (onEnemyDestroy != null)
-        {
-            onEnemyDestroy();
-        }
-    }
+//    //public event Action onEnemyDestroy;
+//    //public void DestroyEnemy()
+//    //{
+//    //    if (onEnemyDestroy != null)
+//    //    {
+//    //        onEnemyDestroy();
+//    //    }
+//    //}
    
-}
+//}
