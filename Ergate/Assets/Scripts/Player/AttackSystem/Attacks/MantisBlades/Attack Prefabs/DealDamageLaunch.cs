4877d52c51c4d6b50baf6e6bffcb6682d5c0a3db
Sadzony <szymon.sadunski@gmail.com>
2022-05-24 18:03:19 +0100

    prefab edit 1

--- Ergate/Assets/Scripts/Player/AttackSystem/Attacks/MantisBlades/Attack Prefabs/DealDamageLaunch.cs
+++ Ergate/Assets/Scripts/Player/AttackSystem/Attacks/MantisBlades/Attack Prefabs/DealDamageLaunch.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class DealDamageLaunch : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float damage;
+    public float knockPower;
+    private List<GameObject> damagedEnemies;
+    public bool overrideDirection = false;
+    public Vector3 newDirection;
+    private void Start()
     {
-        
+        damagedEnemies = new List<GameObject>();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.tag == "Enemy" && !damagedEnemies.Contains(other.transform.parent.gameObject))
+        {
+            GameObject enemy = other.transform.parent.gameObject;
+            BaseEnemy enemyScript = enemy.GetComponent<BaseEnemy>();
+            damagedEnemies.Add(enemy);
+            Vector3 knockbackForceDirection;
+            if (!overrideDirection)
+            {
+                Vector3 positionNoY = new Vector3(transform.position.x, enemy.transform.position.y, transform.position.z);
+                knockbackForceDirection = enemy.transform.position - enemyScript.m_Target.transform.position;
+            }
+            else
+            {
+                knockbackForceDirection = transform.rotation * newDirection;
+            }
+            knockbackForceDirection.Normalize();
+            enemy.GetComponent<NavMeshAgent>().enabled = false;
+            enemyScript.rb.velocity = new Vector3(0, 0, 0);
+            enemyScript.rb.AddForce(knockbackForceDirection * knockPower, ForceMode.Impulse);
+            enemyScript.TakeDamage(damage);
+        }
     }
 }
