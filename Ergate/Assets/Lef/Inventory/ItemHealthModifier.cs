c8f24de8b99a87afe583932b77e1ff9a7399388b
lefk36 <57408683+lefk36@users.noreply.github.com>
2022-05-26 12:21:26 +0100

    dropRate

--- Ergate/Assets/Lef/Inventory/ItemHealthModifier.cs
+++ Ergate/Assets/Lef/Inventory/ItemHealthModifier.cs
@@ -7,7 +7,11 @@ public class ItemHealthModifier : CharacterModifications
 {
     public override void AffectCharacterStats(GameObject character, float val)
     {
-        //HealthBar.instance.AddHealth((int)val);
+        float health;
+        health = character.GetComponent<PlayerPoiseAndHealth>().m_currentPlayerHealth;
+
+        health += val;
+        HealthBar.instance.AddHealth(val);
         Debug.Log("Heal Amount : " + val);
     }
 }
