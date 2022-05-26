d1dc2dc37170820c58db03e32a43496c831a8540
adrian haythorne <71699779+adrian-1066@users.noreply.github.com>
2022-05-26 12:22:28 +0100

    ///

---
+++ Ergate/Assets/adriansFolder/adrianScripts/winConditions/winCondition.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class winCondition : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
